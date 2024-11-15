89205333208db7b520cdb0db30216cf2103e0b9a
BiniyamTafesse <biniyamtafesse228@gmail.com>
2024-11-16 00:05:33 +0300

    profile

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+// import reactLogo from './assets/react.svg'
+
+import './App.css'
+import Profile from './Profile'
+
+function App() {
+
+  return (
+    <>
+     <Profile />
+    </>
+  )
+}
+
+export default App
